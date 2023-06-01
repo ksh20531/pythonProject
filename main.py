da16ad87ba4fb43ebda7edc87ed2cafd72522fd1
dodong.kim <ksh20531@naver.com>
2023-06-01 23:52:36 +0900

    add timer

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
-import json
-import sunVally_config as info
+import sunVally_config as sunV
+import ssl
+import requests
+from datetime import datetime
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.alert import Alert
-from time import sleep
-import requests
-import urllib.request
-import ssl
-from bs4 import BeautifulSoup
 from selenium.webdriver.support.wait import WebDriverWait
-from datetime import datetime
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def makeTarget():
+    data = {}
+    now = datetime.now()
+    nine = datetime(now.year, now.month, now.day, 9, 00)
+
+    if nine > now:
+        nine = datetime(now.year, now.month, now.day, 9, 30)
+        data[0] = sunV.ilJuk
+        data[1] = sunV.timeIljuk
+
+        if nine > now:
+            nine = datetime(now.year, now.month, now.day, 10, 30)
+        data[0] = sunV.yeoJu
+        data[1] = sunV.timeYeoju
+    else:
+        data[0] = sunV.sulAk
+        data[1] = sunV.timeSulak
+        return data
+
+
+target = makeTarget()[0]
+targetTime = datetime.strptime(makeTarget()[1], '%H:%M:%S').time()
+print(type(targetTime), "targetTime :", targetTime)
 
 # ssl 오류 발생 방지용
 ssl._create_default_https_context = ssl._create_unverified_context
-
-target = info.sulAk
-
-# 네이버 시계
-# 실시간으로 계속 받아올 수 있게 수정해야 함
-naverUrl = 'http://www.naver.com'
-date = urllib.request.urlopen(naverUrl).headers['Date'][5:-4]
-hour, min, sec = date[12:14], date[15:17], date[18:]
 
 # 크롬 브라우저 꺼짐 방지 옵션
 chrome_options = Options()
@@ -31,76 +45,86 @@ driver.maximize_window()  # 창 최대화 옵션
 driver.implicitly_wait(10)  # 페이지 로드 시간 옵션
 
 # 페이지 오픈
-driver.get(info.url_login)
+driver.get(sunV.url_login)
+
 
 # 아이디,비밀번호 입력 후 로그인 클릭
-driver.find_element(By.ID, "usrId").send_keys(info.my_id)
-driver.find_element(By.ID, "usrPwd").send_keys(info.my_pw)
+driver.find_element(By.ID, "usrId").send_keys(sunV.my_id)
+driver.find_element(By.ID, "usrPwd").send_keys(sunV.my_pw)
 driver.find_element(By.ID, "fnLogin").click()
 
 # alert 창 처리를 위한 sleep
-sleep(0.5)
+# sleep(0.5)
 
 # alert 창처리
 try:
+    WebDriverWait(driver, 5).until(EC.alert_is_present())
     alert = driver.switch_to.alert
     alert.accept()
     print('alert ok')
 except:
-    alert = driver.switch_to.alert.dismiss()
+    WebDriverWait(driver, 5).until(EC.alert_is_present())
+    alert = driver.switch_to.alert
+    alert.dismiss()
     print('alert error')
     pass
 
-# 테스트 필요
-# Alert(driver).accept()
-# Alert(driver).dismiss()
-#
-# print(Alert(driver).text)
-# Alert(driver).send_keys(keysToSend=Keys.ESCAPE)
 
 # 예약 페이지로 이동
-driver.get(info.url_reservation)
+driver.get(sunV.url_reservation)
 print("move to reserve page")
-driver.find_element(By.ID, info.btnId+target).click()
+driver.find_element(By.ID, sunV.btnId + target).click()
 print("move to target")
 
-# 정해진 시간에 새로고침 되게 수정 해야 함.
+# 예약시간 timer
+while True:
+    date = datetime.now().strftime('%H:%M:%S')
+    time = datetime.strptime(date, '%H:%M:%S').time()
+    sleep(0.5)
+    print('type :', type(time), "time : ", time)
 
+    if time == targetTime:
+        driver.refresh()
 
-# 새로고침
-# driver.refresh()
+        # 날짜 선택
+        req = requests.get(sunV.url_reservation + '?sel=' + target)
+        if req.status_code == 200:
+            try:
+                date = "A"+sunV.reserveDate
+            except:
+                date = "B"+sunV.reserveDate
 
-# 날짜 선택
-req = requests.get(info.url_reservation+'?sel='+target)
-if req.status_code == 200:
-    try:
-        date = "A"+info.reserveDate
-    except:
-        date = "B"+info.reserveDate
-    print(date)
-    driver.find_element(By.ID, date).click()
-else:
-    print("date select error")
+            print('selected date is ',date)
+            driver.find_element(By.ID, date).click()
+        else:
+            print("date select error")
 
-req2 = requests.get(info.url_ajax)
-print("ajax status :", req2.status_code)
+        # 시간 선택
+        req2 = requests.get(sunV.url_ajax)
+        print("ajax status :", req2.status_code)
 
-if req2.status_code == 200:
-    elements = driver.find_elements(By.CSS_SELECTOR, '#tabCourseALL > div > div > table > tbody > tr')
-    targetTime = datetime.strptime(info.reserveTime, '%H:%M')
+        if req2.status_code == 200:
+            elements = driver.find_elements(By.CSS_SELECTOR, '#tabCourseALL > div > div > table > tbody > tr')
+            reserveTime = datetime.strptime(sunV.reserveTime, '%H:%M:%S')
 
-    for e in elements:
-        # 예약 시간 datetime형식으로 변환 후 차이 계산
-        resTime = datetime.strptime(e.text.split(' ')[3], '%H:%M')
-        diff = resTime - targetTime
-        diff = int(diff.seconds / 60)  # datetime to int
+            for e in elements:
+                # 예약 시간 datetime형식으로 변환 범위 계산
+                resTime = datetime.strptime(e.text.split(' ')[3], '%H:%M')
+                diff = resTime - reserveTime
+                diff = int(diff.seconds / 60)  # second to minute
 
-        if (resTime >= targetTime and diff <= info.timeRange):
-            print(e.text)
-            e.find_element(By.CLASS_NAME, 'btn-res').click()
-            break
+                # 예약시간 이상이고, 범위 안에 들었을 경우
+                if (resTime >= reserveTime and diff <= sunV.timeRange):
+                    if e.text.find('마감') < 0:
+                        e.find_element(By.CLASS_NAME, 'btn-res').click()
+                        print("move to select page")
+                        break
+            try:
+                driver.find_element(By.CLASS_NAME, 'btn-res03').click()
+                print('success')
+            except:
+                print('fail')
+                pass
+        break
 
-    print("move to select page")
-    driver.find_element(By.CLASS_NAME, 'btn-res03').click()
-
-print("end")
+print("program end")
--- test.py
+++ test.py
@@ -1,26 +1,84 @@
 from datetime import datetime
-import sunVally_config as info
-import datetime
-
-resTime = datetime.datetime.strptime('06:25', '%H:%M')
-resTime2 = datetime.datetime.strptime('11:40', '%H:%M')
-targetTime = datetime.datetime.strptime(info.reserveTime, '%H:%M')
-
-che = resTime - targetTime
-che2 = resTime2 - targetTime
-print(che)
-print(che2)
-
-if resTime <= targetTime:
-    print("kkkk")
-else:
-    print("dddd")
-
-# test = int(che.seconds / 60)
-# if test >= 10:
-#     print(test)
+import sunVally_config as sunV
+from time import sleep
+import urllib.request
+import ssl
+from pytz import timezone, utc
+import threading
 
 
+# date = datetime.now().strftime('%H:%M:%S')
+# time = datetime.strptime(date, '%H:%M:%S').time()
+# print('date :', type(date), date)
+# print('time :', type(time), time)
+#
+# test = datetime.strptime(sunV.date, '%H:%M:%S').time()
+# print('test :', type(test), test)
+
+#
+#
+# end = False
+# while not end:
+#     date = datetime.now().strftime('%H:%M:%S')
+#     kkk = datetime.strptime(date, '%H:%M:%S').time()
+#     sleep(0.5)
+#     if kkk == test:
+#         print("same")
+#         break
+#     else:
+#         test1()
+#         print(date)
+# print('end')
+
+# result = []
+#
+#
+# def printer(message):
+#     print(message)
+#
+#
+# def tim():
+#     while True:
+#         time = datetime.now().time()
+#         result.append(time)
+#         printer(time)
+#         if time == datetime.strptime(sunV.date, '%H:%M:%S').time():
+#             print("same")
+#             break
+#         sleep(1)
+#
+#
+# th1 = threading.Thread(target=tim, args=())
+# th1.start()
+#
+# print('result3 :', result)
+# print(result[0])
+# sleep(3)
+# print("sleep 3 sec")
+# print(result[0])
 
 
 
+# sleep 걸고 시간 받아오는 것 차이 확인용
+# sec = datetime.now().time()
+# print(sec)
+# sleep(2)
+# print(sec)
+#
+# print(datetime.now().time())
+# sleep(2)
+# print(datetime.now().time())
+
+
+# 시간 계산
+# today = datetime.now()
+# test = datetime.strptime(sunV.date, '%Y-$M-%d %H:%M:%s')
+# print(type(today), today)
+# print(type(test), test)
+# diffTime = today - test
+# print(diffTime)
+# end = False
+# while not end:
+#     today = datetime.now()
+#     if today.time() >= datetime.strptime(info.timeSulak, '%H:%M'):
+#         print(1)
